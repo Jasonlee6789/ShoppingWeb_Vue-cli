f153d2920d8c4d7f588eea12485f1527acf5a56e
Jasonlee6789 <jeffersonlee@163.com>
2020-08-19 19:00:22 +0800

    I want to build a shopping web demo with vue-cli.I customsize the port and router. at first.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+//@ => webpack resolve => /src 方法不同的地方去引用 src 目录
+import "@/assets/static/css/css.css";
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  //Render函数是Vue2.x版本新增的一个函数；
+  // 使用虚拟dom来渲染节点提升性能，因为它是基于JavaScript计算。
+  // 通过使用createElement(h)来创建dom节点。
+  //createElement,可以写成h是render的核心方法。
+  //其Vue编译的时候会把template里面的节点解析成虚拟dom；
+  render: (h) => h(App),
+}).$mount("#app");
