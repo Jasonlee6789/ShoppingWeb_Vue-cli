a4e931f07efb5c62f0e15c63f6e51e707bd541cb
Jasonlee6789 <jeffersonlee@163.com>
2020-08-21 10:14:20 +0800

    I use v-model to practise the clooect submitData.username and .password.

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,46 +1,58 @@
 <template>
-<div id="main">
-        
-       <div id="login" class="panel">
-           <h2>登录</h2>
-           <form action="">
-                <div class="form-item">
-                    <label>
-                        <span class="txt">姓名：</span>
-                        <input type="text" class="form-input" ref="username">
-                    </label>
-                </div>
-                <div class="form-item">
-                    <label>
-                        <span class="txt">密码：</span>
-                        <input type="password" class="form-input" ref="password">
-                    </label>
-                </div>
+  <div id="main">
+    <div id="login" class="panel">
+      <h2>登录</h2>
+      <form action>
+        <div class="form-item">
+          <label>
+            <span class="txt">姓名：</span>
+            <!-- <input type="text" class="form-input" ref="username" /> -->
+            <input type="text" class="form-input" v-model="submitData.username" />
+          </label>
+        </div>
+        <div class="form-item">
+          <label>
+            <span class="txt">密码：</span>
+            <!-- <input type="password" class="form-input" ref="password" /> -->
+            <input type="password" class="form-input" v-model="submitData.password" />
+          </label>
+        </div>
 
-                <div class="form-item">
-                    <label>
-                        <span class="txt"></span>
-                        <button class="form-button primary" @click.prevent="doLogin">登录</button>
-                        <button class="form-button">注册</button>
-                    </label>
-                </div>
-           </form>
-       </div>  
-</div>  
+        <div class="form-item">
+          <label>
+            <span class="txt"></span>
+            <button class="form-button primary" @click.prevent="doLogin">登录</button>
+            <button class="form-button">注册</button>
+          </label>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Login',
+  name: "Login",
+  data() {
+    return {
+      submitData: {
+        usename: "",
+        password: "",
+      },
+    };
+  },
+  methods: {
+    doLogin() {
+      //收集用户填写的用户名和密码
+      console.log(
+        "收集用户填写的用户名和密码",
+        this.$submitData
+        // this.$refs.username.value,
+        // this.$refs.password.value
+      );
 
-    methods: {
-        doLogin(){
-            //收集用户填写的用户名和密码
-console.log('收集用户填写的用户名和密码',this.$refs.username.value,
-this.$refs.password.value);
-
-            //发送ajax请求
-        }
-}
-}
+      //发送ajax请求
+    },
+  },
+};
 </script>
